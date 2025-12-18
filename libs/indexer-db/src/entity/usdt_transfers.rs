1d024ccc1ad60f686a9866cb0a6ae1f8cd0c4675
matheeshame <hpraveenmatheesha@gmail.com>
2025-12-18 18:23:19 +0530

    Refactor USDT processing to improve error handling and database integration
    - Introduced new error variants in `AppError` for enhanced validation.
    - Updated `process_logs` to ensure `CHAIN_ID` is retrieved from environment variables.
    - Modified `ContractHandler` to incorporate database connection and chain ID in event processing.
    - Improved `USDTErc20` to manage USDT transfers and approvals with database support.
    - Added conversion from U256 to BigDecimal for precise value handling in transfers.

---
+++ libs/indexer-db/src/entity/usdt_transfers.rs
@@ -0,0 +1,54 @@
+use sqlx::{Executor, Postgres, types::{BigDecimal, chrono}};
+
+#[derive(sqlx::FromRow,Debug)]
+pub struct UsdtTransfers{
+    pub id: i64,
+    pub tx_hash: [u8; 32],
+    pub block_number: BigDecimal,
+    pub block_timestamp: chrono::NaiveDateTime,
+    pub from_address: [u8; 20],
+    pub to_address: [u8; 20],
+    pub value: sqlx::types::BigDecimal,
+    pub classification: i16,
+    pub chain_id: i64,
+    pub created_at: chrono::NaiveDateTime
+}
+impl UsdtTransfers {
+    pub async fn create<'c, E>(transfer: UsdtTransfers, connection: E) -> Result<UsdtTransfers, sqlx::Error>
+    where
+        E: Executor<'c, Database = Postgres>,
+    {
+        let query = r#"
+            INSERT INTO usdt_transfers (tx_hash, block_number, block_timestamp, from_address, to_address, value, classification, chain_id)
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+            RETURNING *
+        "#;
+        sqlx::query_as::<_, UsdtTransfers>(query)
+            .bind(transfer.tx_hash)
+            .bind(transfer.block_number)
+            .bind(transfer.block_timestamp)
+            .bind(transfer.from_address)
+            .bind(transfer.to_address)
+            .bind(transfer.value)
+            .bind(transfer.classification)
+            .bind(transfer.chain_id)
+            .fetch_one(connection)
+            .await
+    }
+
+
+    pub async fn fetch_by_id<'c, E>(id: u64, connection: E) -> Result<UsdtTransfers, sqlx::Error>
+    where
+        E: Executor<'c, Database = Postgres>,
+    {
+        let query = "SELECT * FROM usdt_transfers WHERE id = $1";
+
+        sqlx::query_as::<_, UsdtTransfers>(query)
+            .bind(id as i64)
+            .fetch_one(connection)
+            .await
+    }
+    // pub async fn upate_usdt_transfer<'c,E>(&self,tx_hash: Vec<u8>,connection: E) -> Result<UsdtTransfers,sqlx::Error> where E: Executor<'c,Database=Postgres>{
+    //     let query = "UPDATE usdt_transfers SET"
+    // }
+}
