a1102d52d562b5a92a59fcba8dfcd334118cf928
matheeshame <hpraveenmatheesha@gmail.com>
2025-12-20 12:04:35 +0530

    Fix: handle duplicate transfers

--- libs/indexer-db/src/entity/usdt_transfers.rs
+++ libs/indexer-db/src/entity/usdt_transfers.rs
@@ -19,10 +19,11 @@ impl UsdtTransfers {
         E: Executor<'c, Database = Postgres>,
     {
         let query = r#"
-            INSERT INTO usdt_transfers (tx_hash, block_number, block_timestamp, from_address, to_address, value, classification, chain_id)
-            VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
-            RETURNING *
-        "#;
+        INSERT INTO usdt_transfers (tx_hash, block_number, block_timestamp, from_address, to_address, value, classification, chain_id)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+        ON CONFLICT (tx_hash, from_address, to_address, value, block_number) DO NOTHING
+        RETURNING *
+    "#;
         println!("query {}",query);
         sqlx::query_as::<_, UsdtTransfers>(query)
             .bind(transfer.tx_hash)
